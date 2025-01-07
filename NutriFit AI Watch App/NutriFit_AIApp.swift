49398ebce9de443a63d788b887af174c59e75b4c
Sarim <sarimk80@gmail.com>
2025-01-07 10:43:29 +0500

    Initial Commit

---
+++ NutriFit AI Watch App/NutriFit_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFit_AIApp.swift
+//  NutriFit AI Watch App
+//
+//  Created by sarim khan on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NutriFit_AI_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
