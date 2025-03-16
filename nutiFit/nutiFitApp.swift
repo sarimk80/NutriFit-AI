4427f8fdedc4e1b082348cc98f3a60671d303112
Sarim <sarimk80@gmail.com>
2025-03-16 14:33:40 +0500

    initial commit

---
+++ nutiFit/nutiFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutiFitApp.swift
+//  nutiFit
+//
+//  Created by sarim khan on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct nutiFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
